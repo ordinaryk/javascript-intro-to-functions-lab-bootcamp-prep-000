9a2132d56f73af6428ec611f2fec8d6d222eca2d
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-27 17:49:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string === string.toLowerCase())
+  if(string.toLowerCase())
   return "I can't hear you!"
 }
 
 function sayHiToGrandma(string) {
-  if(string === string.toUpperCase())
+  if(string.toUpperCase())
   return "YES INDEED!"
 }
 
